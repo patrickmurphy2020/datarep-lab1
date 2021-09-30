281c712848ad397eb73f57e1861aa899307218e5
Patrick <g00379725@gmit.ie>
2021-09-30 11:35:52 +0100

    First Commit

---
+++ BMI.js
@@ -0,0 +1,15 @@
+class BMI {
+    constructor(height, weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    calculateBMI() {
+        let bmi = this.weight / (this.height ** 2);
+        return bmi;
+    }
+}
+
+let MyBmi = new BMI(2,90);
+let calaculateBMI = MyBmi.calculateBMI();
+console.log(calaculateBMI);
